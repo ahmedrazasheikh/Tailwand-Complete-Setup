1c1c1e11f7abf0e51627c1708f550ecf362913ad
Ahmed Raza <bhaia9036@gmail.com>
2022-12-17 05:11:45 -0800

    initial commit

---
+++ web/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+<h1  className='text-3xl font-bold underline' >Ahmed Raza</h1>
+
+<p  className='text-red-400' >
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam consequatur, doloremque et cupiditate at porro. Veritatis expedita rerum maiores id beatae? Odit debitis illum, ipsam tempora quaerat dolor labore non.
+</p>
+    </div>
+  )
+}
+
+export default App
